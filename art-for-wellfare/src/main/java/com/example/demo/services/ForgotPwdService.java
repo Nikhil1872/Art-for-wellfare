a454e01b8a9caa8e891183a5c2efd9b92ef05b5f
Nikhil <ntalmale07@gmail.com>
2023-08-22 12:29:21 +0530

    react and springboot

---
+++ art-for-wellfare/src/main/java/com/example/demo/services/ForgotPwdService.java
@@ -0,0 +1,35 @@
+package com.example.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Question;
+import com.example.demo.repositories.ForgotpwdRepository;
+//import com.example.demo.repository.ForgotpwdRepository;
+
+@Service
+public class ForgotPwdService {
+	
+	@Autowired
+	ForgotpwdRepository frepo;
+	
+	public String getQuestionText(String email)
+	{
+		String q;
+		System.out.println(email);
+		Optional<String> ol = frepo.getQueTextByEmail(email);
+		try
+		{
+			q=ol.get();
+		}
+		catch(Exception e)
+		{
+			q=null;
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return q;
+	}
+}
